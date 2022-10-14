6ded15bb03f09d0554fe18fc5185d598bd85f3b3
TomChen <tom.chen@tpisoftware.com>
2022-10-14 12:00:53 +0800

    initialize myMall

---
+++ myMall/src/test/java/com/review/myMall/MyMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.review.myMall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
